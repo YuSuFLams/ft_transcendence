259c39d3f95a2272f2e88d9cbe7f2faee82513b2
Badr Eddine El Oirghi <bel-oirg@e1r13p4.1337.ma>
2025-01-29 23:04:46 +0100

    auth is working now

--- backend/users/serializer.py
+++ backend/users/serializer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Account
-
+import pyotp
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -11,11 +11,47 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ['first_name', 'last_name','email', 'username', 'password', 'repassword']
 
     def create(self, validated_data):
-        user = Account(email=validated_data['email'],
-                    username=validated_data['username'],
-                    first_name=validated_data['first_name'],
-                    last_name=validated_data['last_name'])
-        #TODO you can add list.get('key', 'default_value')
+        user = Account(email=validated_data.get('email'),
+                    username=validated_data.get('username'),
+                    first_name=validated_data.get('first_name'),
+                    last_name=validated_data.get('last_name'),
+                    otp_secret=pyotp.random_base32())
         user.set_password(validated_data['password'])
         user.save()
         return user
+
+
+########## # OTP   [DO NOT REMOVE IT] USED ALSO ON JWT SETTINGS
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        totp = pyotp.TOTP(user.otp_secret)
+        user.otp_code = totp.now()
+        user.is_otp_verified = False
+        user.save()
+        print(f'FROM GET TOKEN {user.otp_code}')
+        send_otp(user)
+        return token
+        
+
+from django.core.mail import send_mail
+from django.conf import settings
+import smtplib
+
+def send_otp(user):
+    try:
+        send_mail(f'Your Pong code is {user.otp_code}',
+            f'Hey {user.username}, Please Enter the code below to verify your login \n{user.otp_code}',
+            settings.EMAIL_HOST_USER,
+            [user.email],
+            fail_silently=False)
+        return True
+    except smtplib.SMTPException as e:
+        print('[-] Failed to send the OTP code ' + e)
+        return False
+
+class OTPSerializer(serializers.Serializer):
+    form_OTP = serializers.CharField(required=True, max_length=6)
